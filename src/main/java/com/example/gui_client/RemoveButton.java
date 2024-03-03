40e2ced76fdc15a6308b904d544610a13f666556
Oliver-fits7 <150403145+Oliver-fits7@users.noreply.github.com>
2024-03-03 15:23:31 +0000

    everything else
    all the stuff I forgot to include from last commit

---
+++ src/main/java/com/example/gui_client/RemoveButton.java
@@ -0,0 +1,24 @@
+package com.example.gui_client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RemoveButton extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RemoveButton.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        stage.setTitle("Class Scheduler");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
