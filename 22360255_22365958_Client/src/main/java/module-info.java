7c7fe8029b6cbeaa01fcb9250791f2415eb9c575
Ben Burt <benjaminvicenteburt@gmail.com>
2024-03-03 15:26:09 +0000

    Fixing File Structure

--- 22360255_22365958_Client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.mycompany.client to javafx.fxml;
-    exports com.mycompany.client;
-}
